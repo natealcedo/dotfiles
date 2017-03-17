f7cd7399f6e2958880cbb5615a9aa3f274b74998
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-03-17 13:53:47 +0800

    use local installation of eslint

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim eslintrc.json gitconfig tern-config"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim gitconfig tern-config"    # list of files/folders to symlink in homedir
 
 ##########
 
