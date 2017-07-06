5870dae7af8553938ec8d7962b952600e0637184
Nathaniel D Alcedo Jr <nathanielalcedojr@gmail.com>
2017-07-07 07:25:21 +0800

    Add snippet transfer script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -24,8 +24,12 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+  echo "Moving any existing dotfiles from ~ to $olddir"
+  mv ~/.$file ~/dotfiles_old/
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
 done
+
+echo "Transferring Snippets"
+cd ~/dotfiles/vim/plugged/ && mkdir UltiSnips/ && cd ~/dotfiles && cp ./javascript.snippets ~/dotfiles/plugged/UltiSnips
+echo "Done"
