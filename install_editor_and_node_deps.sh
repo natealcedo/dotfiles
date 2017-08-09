070c233e981633fa16c6025242c3dbc7c2fdb192
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2017-08-09 12:52:19 +0800

    Wip for installing editor and node deps

---
+++ install_editor_and_node_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Beginning the installation of node dependencies"
+
+#TODO Get the script from nodejs.org to install node
+sudo npm install -g eslint eslint-plugin-import eslint-plugin-react babel-eslint
+echo "=================================================="
+
+echo "Installing pip dependencies for vim"
+pip install flake-8 autopep8
+echo "=================================================="
+echo
+echo "DONE"
+
+
