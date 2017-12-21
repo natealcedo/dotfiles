a14b1f1b5c9e968d74fbe4bc1d2fdb0f42183d21
Nathaniel D Alcedo Jr <ndaljr@gmail.com>
2017-12-21 15:41:38 +0200

    Install nodejs from bashscript

--- install_editor_and_node_deps.sh
+++ install_editor_and_node_deps.sh
@@ -2,7 +2,8 @@
 
 echo "Beginning the installation of node dependencies"
 
-#TODO Get the script from nodejs.org to install node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo npm install -g eslint eslint-plugin-import eslint-plugin-react babel-eslint prettier
 echo "=================================================="
 
